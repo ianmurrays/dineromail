59176fc390c875768a10fa49dfdff8f609182238
Ian Murray <ianmurrays@gmail.com>
2011-09-05 17:26:50 -0300

    Fixes currency bug

--- lib/dineromail/configuration.rb
+++ lib/dineromail/configuration.rb
@@ -3,8 +3,8 @@ module Dineromail
     attr_accessor :payment_url, :ipn_webservice_url, :account_number, :password,
               :logo_url, :return_url, :error_url, :pending_url, :pay_methods, :currency, :button_image_url
             
-    PESO = 1
-    DOLLAR = 2
+    PESO = 'CLP'
+    DOLLAR = 'USD'
   end
 
   class << self
